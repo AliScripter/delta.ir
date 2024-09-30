d68480e6d704fee9b8aa2cbc4cc3f75fe476dc1e
John Doe <johndoe@example.com>
2024-09-30 15:02:21 +0330

    fix bootstrap-vue plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
     <div>
-      
-        <NuxtPage />
+        <main>
+            <!-- <TopNavigationBar2 /> -->
+
+            <!-- <Hero /> -->
+
+            <About />
+            <!-- <pagesDemosCreativeAgencyComponentsAbout /> -->
+            <!-- <Services /> -->
+
+            <!-- <RecentProjects /> -->
+
+            <!-- <Testimonials /> -->
+
+            <!-- <FAQ /> -->
+
+            <!-- <Blogs /> -->
+        </main>
+
+        <!-- <Footer2 /> -->
     </div>
 </template>
-<script></script>
+<script setup>
+import About from '../pages/demos/CreativeAgency/components/About.vue';
+// import Hero from '../pages/demos/CreativeAgency/components/Hero.vue';
+// import Services from '../pages/demos/CreativeAgency/components/Services.vue';
+// import Blogs from '../pages/demos/CreativeAgency/components/Blogs.vue';
+// import FAQ from '../pages/demos/CreativeAgency/components/FAQ.vue';
+// import Testimonials from '../pages/demos/CreativeAgency/components/Testimonials.vue';
+// import RecentProjects from '../pages/demos/CreativeAgency/components/RecentProjects.vue';
+// import Footer2 from '../pages/demos/CreativeAgency/components/Footer2.vue';
+// import TopNavigationBar2 from '../pages/demos/CreativeAgency/components/TopNavigationBar2.vue';
+</script>
 <style></style>
